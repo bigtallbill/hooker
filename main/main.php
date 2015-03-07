1c486da11994989f5a96e06d755ed868e203d065
bigtallbill <bigtallbill@gmail.com>
2015-03-07 12:05:02 -0500

    Use symfony/console for all command actions

--- main/main.php
+++ main/main.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 /**
  * Created by PhpStorm.
@@ -21,81 +22,7 @@ $loader = new \Composer\Autoload\ClassLoader();
 $loader->add('Bigtallbill', HOOKER_ROOT . '/src');
 $loader->register();
 
-$sLog = new SimpleLog();
-$cmdPrompt = new CommandPrompt();
-
-//--------------------------------------
-// Check arguments and display
-// help options
-//--------------------------------------
-
-if (count($argv) <= 1) {
-    showHelp();
-}
-
-// get command argument
-$command = $argv[1];
-
-// look for utility commands
-switch ($command) {
-    case 'help':
-    case '--help':
-    case '-help':
-    case '-h':
-        showHelp();
-        break;
-}
-
-// get required args
-$repoRoot     = ArgUtils::getArgPair($argv, '-p', null, true);
-$repoRoot = $repoRoot['-p'];
-
-// merge all of the arguments together for easy reference
-//$arguments = array_merge($algo, $mode, $pass, $file, $hashAlgo);
-
-
-//--------------------------------------
-// MAIN COMMANDS
-//--------------------------------------
-
-switch ($command) {
-    case 'install':
-
-
-        if ($repoRoot === null) {
-            $repoRoot = getcwd();
-        }
-
-        $sLog->log("using current working directory: $repoRoot");
-
-        $hk = new Hooker(HOOKER_ROOT, $repoRoot);
-        $hk->install();
-
-        break;
-    case 'execute':
-
-        $hk = new Hooker(HOOKER_ROOT, getcwd());
-
-        if (($out = $hk->execute($argv[2], $argv)) !== true) {
-            list($output, $exitCode) = $out;
-            echo $output . PHP_EOL;
-            exit($exitCode);
-        }
-
-        break;
-    default:
-        showHelp();
-        break;
-}
-
-/**
- * displays the help text
- */
-function showHelp()
-{
-    global $sLog;
-
-    $sLog->log(file_get_contents(dirname(__FILE__) . '/help'));
-
-    exit();
-}
+$app = new \Symfony\Component\Console\Application('hooker');
+$app->add(new \Bigtallbill\Hooker\Commands\CommandInstall(HOOKER_ROOT));
+$app->add(new \Bigtallbill\Hooker\Commands\CommandExecute(HOOKER_ROOT));
+$app->run();
